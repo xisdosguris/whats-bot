c5227be9dd157837c9ec737dda43a289c53208c0
xisdosguris <123604754+xisdosguris@users.noreply.github.com>
2023-01-26 00:24:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-res.send('Hello World!2')
+res.send('Hello World!3')
 // Invocamos o leitor de qr code
 const qrcode = require('qrcode-terminal');
 
 const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // MudanÃ§a Buttons
+const client = new Client();
+
 
 })
 
