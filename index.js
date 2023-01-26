213a473b44bcfe3422a81c37b9b9cf75af20a3bd
xisdosguris <123604754+xisdosguris@users.noreply.github.com>
2023-01-26 00:20:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+res.send('Hello World!')
 // Invocamos o leitor de qr code
 const qrcode = require('qrcode-terminal');
-const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // MudanÃ§a Buttons
-const client = new Client();
-// entao habilitamos o usuario a acessar o serviÃ§o de leitura do qr code
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-// apos isso ele diz que foi tudo certin 
-client.on('ready', () => {
-    res.send('Conectado')
-});
-// E inicializa tudo para fazer a nossa magica =)
-client.initialize();
-
-const delay = ms => new Promise(res => setTimeout(res, ms)); // FunÃ§Ã£o que usamos para criar o delay entre uma aÃ§Ã£o e outra
 
 })
 
